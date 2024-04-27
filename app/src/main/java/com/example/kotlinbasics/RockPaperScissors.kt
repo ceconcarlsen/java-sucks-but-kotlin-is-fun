016122d924d0ef8e4364478a3887e42f68103c3f
Gabriel <gabriel.cecon@mblabs.com.br>
2024-04-27 15:06:36 -0300

    while, when

---
+++ app/src/main/java/com/example/kotlinbasics/RockPaperScissors.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinbasics
+
+import android.widget.Switch
+
+fun main() {
+//    var computerChoice = ""
+//    var playerChoice = ""
+//
+//    println("Rock, Paper or Scissors? Enter your choice");
+//
+//    playerChoice = readln()
+//
+//    println(playerChoice)
+//
+//    val choice = when((1..3 ).random()) {
+//        1 -> "Rock"
+//        2 -> "Paper"
+//        3 -> "Scissors"
+//        else -> "Invalid choice"
+//    }
+//
+//    println(choice)
+
+
+    //WHILE
+    var count = 0
+    while(count <= 3) {
+        print(count)
+        count++
+    }
+}

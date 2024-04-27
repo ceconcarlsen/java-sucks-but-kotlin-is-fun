72bdfebbc64cbe8f7ee1e6d30142d317990fffd2
Gabriel <gabriel.cecon@mblabs.com.br>
2024-04-27 14:18:41 -0300

    float, double, unsigned integers

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -11,4 +11,16 @@ fun main(){
     println(myAge);
 
     var shortNumber: Short = 20001
+
+    val pi = 3.14 //Double, but we can type cast to Float when 3.14f. Double is more precise
+
+    // Hint: Remember, float has a limitation on the precision it can store.
+
+    // Unsigned Integer Types - POSITIVE
+    //UByte = 8-bit integer. 0 - 255
+    //UShort = 16-bit
+    //UInt = 32-bit
+    //ULong = 64-bit
+
+
 }

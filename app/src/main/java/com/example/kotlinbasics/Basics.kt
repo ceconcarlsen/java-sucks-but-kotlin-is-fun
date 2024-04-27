05ddc3da84aa376df0447154f2c665699d2c54b4
Gabriel <gabriel.cecon@mblabs.com.br>
2024-04-27 01:07:16 -0300

    byte and short types

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    // val for const
+    // var for let
+
+    println("hello");
+
+    var myAge: Byte = 35 // Byte is a smaller int
+    println(myAge);
+
+    var shortNumber: Short = 20001
+}
